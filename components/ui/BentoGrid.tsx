755613ac6eca2d04009c9a1adb04c622a6c1bcc1
Prakhar <prkbhtt@gmail.com>
2026-02-15 13:49:52 +0530

    PortFolio Version 1

---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto grid w-full max-w-7xl grid-cols-1 gap-4",
+        "sm:grid-cols-2",
+        "lg:grid-cols-3",
+        "auto-rows-[16rem] sm:auto-rows-[18rem] lg:auto-rows-[22rem] xl:auto-rows-[24rem]",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  header,
+  icon,
+  image, // NEW
+  id,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  image?: string; // NEW
+  id: number;
+}) => {
+  return (
+    <div
+      className={cn(
+        "group/bento relative shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/20 dark:bg-black dark:shadow-none",
+        className,
+      )}
+    >
+      {header}
+
+      {/* IMAGE */}
+      {image && (
+        <div className="-mx-4 -mt-4 h-[55%] lg:h-[75%] sm:h-[70%]">
+          <img
+            src={image}
+            alt="card"
+            className="w-full h-full object-cover rounded-t-3xl"
+          />
+        </div>
+      )}
+
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
+        {icon}
+        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+          {title}
+        </div>
+        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
