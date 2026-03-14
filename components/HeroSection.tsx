8229410f604374b6169dd6f20e70e25d3eae9998
Prakhar <prkbhtt@gmail.com>
2026-03-14 19:30:56 +0530

    Hero Section Done

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import MagicButton from './ui/MagicButton';
-import { FaLocationArrow } from 'react-icons/fa6';
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { HoverBorderGradient } from "./ui/hover-border-gradient-button";
+import { Phone } from "lucide-react";
 
 const languages = [
   {
-    name: 'The Greeting',
+    name: "The Greeting",
     code: 'cout << "Hello World" << endl;',
   },
   {
-    name: 'The Logic',
-    code: '“Talk is cheap. Show me the code.”',
+    name: "The Logic",
+    code: "“Talk is cheap. Show me the code.”",
   },
   {
-    name: 'The Craft',
-    code: '“Logic first. Syntax second.”',
+    name: "The Craft",
+    code: "“Logic first. Syntax second.”",
   },
 ];
 
@@ -26,20 +26,20 @@ const HeroSection = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % languages.length);
-    }, 3000); 
+    }, 3000);
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <section className="relative flex flex-col items-center h-full overflow-hidden text-white font-sans">
-      
-      <div className="absolute inset-0 z-0 opacity-20"
+    <section className="relative flex flex-col items-center justify-center text-center h-full overflow-hidden text-white font-sans">
+      <div
+        className="absolute inset-0 z-0 opacity-20"
         style={{
           backgroundImage: `linear-gradient(to right, #1e293b 1px, transparent 1px), 
                             linear-gradient(to bottom, #1e293b 1px, transparent 1px)`,
-          backgroundSize: '40px 40px',
-        }}>
-      </div>
+          backgroundSize: "40px 40px",
+        }}
+      ></div>
 
       <div className="relative z-10 text-center px-4">
         {/* language heading */}
@@ -71,13 +71,17 @@ const HeroSection = () => {
           </AnimatePresence>
         </div>
 
-        <a href="#about">
-                <MagicButton
-                    title="Who I Am"
-                    icon={<FaLocationArrow />}
-                    position="right"
-                />
-            </a>
+        <div className="my-10 flex justify-center text-center">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 cursor-pointer"
+          >
+            {/* <AceternityLogo /> */}
+            <Phone size={18} />
+            <span>Contact Me</span>
+          </HoverBorderGradient>
+        </div>
       </div>
     </section>
   );
